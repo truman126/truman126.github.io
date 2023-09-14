9716339910d0033eb284c32894a8b776d5fafd81
Truman <trumanjohnston@gmail.com>
2023-09-14 15:31:30 -0300

    Fixed overflow on project images

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,10 +2,7 @@ import styled from "styled-components";
 import Card from "react-bootstrap/Card";
 
 function ProjectCard(props) {
-  const Wrapper = styled.div`
   
-
-  `;
   let displayProperty = props.url == "" ? "none" : "default";
 
   let githubButton, demoButton, useButton = "";
@@ -20,8 +17,8 @@ function ProjectCard(props) {
 
 
   return (
-      <Card style={{display:'inline-block', width:'32rem' }}>
-        <Card.Img  style={{height: '20rem', width: '32rem', objectFit: 'scale-down', borderRadius: 'inherit'}} variant="top" src={props.img} />
+      <Card style={{display:'inline-block', width:'32rem',overflow:'hidden' }}>
+        <Card.Img  style={{height: '25rem', width: '32rem', objectFit: 'scale-down', borderRadius: 'inherit'}} variant="top" src={props.img} />
         <Card.Body>
           <Card.Title>{props.title}</Card.Title>
           <Card.Text>{props.desc}</Card.Text>
@@ -29,7 +26,7 @@ function ProjectCard(props) {
           <a class="btn btn-primary" href={props.github} role="button" target="_blank">
             View on Github{" "}
             <img
-              style={{ width: "20px", fill: "red" }}
+              style={{ width: "20px", fill: "red"  }}
               src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
             />
           </a>
